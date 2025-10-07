42d0ee3d919136139ebf01d0191dfdc1eb0f7186
Ян Прескурел <yanpresfoot@gmail.com>
2025-10-07 20:24:43 +0300

    Implementation GetProductAsync, GetProductsAsync methods of ProductService and Implementation GetWarehouseAsync, GetWarehousesAsync methods of WarehouseService

--- Lab6TestTask/Lab6TestTask/Services/Implementations/ProductService.cs
+++ Lab6TestTask/Lab6TestTask/Services/Implementations/ProductService.cs
@@ -1,6 +1,8 @@
 ﻿using Lab6TestTask.Data;
+using Lab6TestTask.Enums;
 using Lab6TestTask.Models;
 using Lab6TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lab6TestTask.Services.Implementations;
 
@@ -19,11 +21,32 @@ public class ProductService : IProductService
 
     public async Task<Product> GetProductAsync()
     {
-        throw new NotImplementedException();
+        if (!await _dbContext.Products.AnyAsync())
+            throw new InvalidOperationException("Таблица Products пуста.");
+
+        var product = await _dbContext.Products
+            .Where(p => p.Status == ProductStatus.Reserved)
+            .OrderByDescending(p => p.Price)
+            .FirstOrDefaultAsync();
+
+        if (product == null)
+            throw new InvalidOperationException("Нет зарезервированных продуктов в базе данных.");
+
+        return product;
     }
 
     public async Task<IEnumerable<Product>> GetProductsAsync()
     {
-        throw new NotImplementedException();
+        if (!await _dbContext.Products.AnyAsync())
+            throw new InvalidOperationException("Таблица Products пуста.");
+
+        var products = await _dbContext.Products
+            .Where(p => p.ReceivedDate.Year == 2025 && p.Quantity > 1000)
+            .ToListAsync();
+
+        if (!products.Any())
+            throw new InvalidOperationException("Нет продуктов, доставленных в 2025 году с количеством больше 1000.");
+
+        return products;
     }
 }
--- Lab6TestTask/Lab6TestTask/Services/Implementations/WarehouseService.cs
+++ Lab6TestTask/Lab6TestTask/Services/Implementations/WarehouseService.cs
@@ -1,6 +1,8 @@
 ﻿using Lab6TestTask.Data;
+using Lab6TestTask.Enums;
 using Lab6TestTask.Models;
 using Lab6TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lab6TestTask.Services.Implementations;
 
@@ -19,11 +21,44 @@ public class WarehouseService : IWarehouseService
 
     public async Task<Warehouse> GetWarehouseAsync()
     {
-        throw new NotImplementedException();
+        if (!await _dbContext.Warehouses.AnyAsync())
+            throw new InvalidOperationException("Таблица Warehouses пуста.");
+
+        if (!await _dbContext.Products.AnyAsync())
+            throw new InvalidOperationException("Таблица Products пуста.");
+
+        var warehouse = await _dbContext.Warehouses
+            .Include(w => w.Products)
+            .OrderByDescending(w => w.Products
+                .Where(p => p.Status == ProductStatus.ReadyForDistribution)
+                .Sum(p => p.Price * p.Quantity))
+            .FirstOrDefaultAsync();
+
+        if (warehouse == null)
+            throw new InvalidOperationException("Не найдено складов с готовыми к распределению продуктами.");
+
+        return warehouse;
     }
 
     public async Task<IEnumerable<Warehouse>> GetWarehousesAsync()
     {
-       throw new NotImplementedException();
+        if (!await _dbContext.Warehouses.AnyAsync())
+            throw new InvalidOperationException("Таблица Warehouses пуста.");
+
+        if (!await _dbContext.Products.AnyAsync())
+            throw new InvalidOperationException("Таблица Products пуста.");
+
+        var warehouses = await _dbContext.Warehouses
+            .Include(w => w.Products)
+            .Where(w => w.Products.Any(p =>
+                p.ReceivedDate.Year == 2025 &&
+                p.ReceivedDate.Month >= 4 &&
+                p.ReceivedDate.Month <= 6))
+            .ToListAsync();
+
+        if (!warehouses.Any())
+            throw new InvalidOperationException("Нет складов, получивших товары во втором квартале 2025 года.");
+
+        return warehouses;
     }
 }
